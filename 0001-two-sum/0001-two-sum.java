7b633b179974a2efe2e5c0fcb4a00916ac07a638
Sudarshan Sarnaik <97461524+SudarshanSarnaik@users.noreply.github.com>
2025-08-24 11:14:55 +0530

    Time: 45 ms (30.88%), Space: 45 MB (64.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    list.add(i);
-                    list.add(j);
-
-                }
+    for (int i = 0; i < nums.length - 1; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return new int[]{i, j};
             }
         }
+    }
+    return new int[]{};
 
-
-
-
-
-
-
-
-       int[] result = new int[list.size()];
-        for (int k = 0; k< list.size(); k++) {
-            result[k] = list.get(k);
-        }
-        
-        return result;
     }
 }
